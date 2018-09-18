1161c775c1b4fe20cdda943e609f6342a7727c84
Aniket Hemant Shinde <aniketshinde.work@gmail.com>
2018-09-18 15:43:06 +0530

    done with users

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost:27017/petShop');
+mongoose.connection.on('error',function(){
+    console.log('error in mongo connection');
+})
+mongoose.connection.on('open',function(){
+    console.log('connected to mongo');
+})
+
+
+const userController = require('./controllers/users');
 
 const app = express();
 
@@ -10,10 +23,16 @@ app.use(bodyParser.urlencoded(
     }
 ))
 
+
 app.get('/',function(req,res){
     res.send('Hello World');
 });
 
+app.post('/api/v1/users', userController.postNewUser);
+app.get('/api/v1/users', userController.getAllUsers);
+app.get('/api/v1/users/:id', userController.getUserById);
+app.put('/api/v1/users/:id', userController.updateUserById);
+app.delete('/api/v1/users/:id', userController.deleteUserById);
 
 app.set('port',3000);
 app.listen(app.get('port'),function(){
